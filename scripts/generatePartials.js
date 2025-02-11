a4ea0a40d76184917189cd3d28a39b5697ba4c80
Daniel Abel <daniel@zkreations.com>
2025-02-11 10:01:33 -0500

    feat: add new Handlebars partials and update build script for partial generation

---
+++ scripts/generatePartials.js
@@ -0,0 +1,42 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import { globSync } from 'glob'
+
+const baseDir = path.join(process.cwd(), 'src/partials')
+const outputDir = path.join(process.cwd(), 'lib/data')
+
+const files = globSync(`${baseDir}/**/*.hbs`)
+
+const partials = {}
+const folders = {}
+
+files.forEach((file) => {
+  const extension = path.extname(file)
+  const fileName = path.basename(file, extension)
+  const partialName = `super.${fileName}`
+  const folderName = path.dirname(file).split(path.sep).pop()
+
+  partials[partialName] = fs.readFileSync(file, 'utf8').trim() + '\n'
+
+  if (!folders[folderName]) {
+    folders[folderName] = []
+  }
+
+  folders[folderName].push(`{{> ${partialName}}}`)
+})
+
+Object.entries(folders).forEach(([folder, partialList]) => {
+  partialList.sort()
+
+  if (partialList.length <= 1) {
+    return
+  }
+
+  const folderPartialName = `super.${folder}`
+  partials[folderPartialName] = partialList.join('\n') + '\n'
+})
+
+const jsContent = `export const superPartials = ${JSON.stringify(partials, null, 2)}\n`
+
+fs.writeFileSync(path.join(outputDir, 'superPartials.js'), jsContent)
+console.log('superPartials.js has been generated successfully!')

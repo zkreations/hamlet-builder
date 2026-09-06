cd37b085474f32b22f35cbb71dac1330770cf51b
Abel <daniel@zkreations.com>
2026-09-06 04:21:28 -0500

    refactor: remove unnecessary JSDoc comments

--- test/helpers/temp.js
+++ test/helpers/temp.js
@@ -2,12 +2,6 @@ import fs from 'node:fs'
 import os from 'node:os'
 import path from 'node:path'
 
-/**
- * Create a temporary directory with a unique prefix and provide a cleanup function.
- *
- * @param {string} [prefix] - Directory name prefix
- * @returns {{ dir: string, cleanup: () => void }} Object containing the created directory path and cleanup function
- */
 export function createTempDir(prefix = 'hamlet-test-') {
   const dir = fs.mkdtempSync(path.join(os.tmpdir(), prefix))
   return {
